e237ce5a37203e39195beb18eae0bf40aee17e4f
MUCHKUNDRAJE <muchkundthote@gmail.com>
2025-10-06 11:44:01 +0530

    added-the-changes

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_2
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtaGJmc2NneWxuaXBqbHhkZnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMzczNzcsImV4cCI6MjA3NDkxMzM3N30.aVf-t6T4Btvq7hp2N64pJXTh34jvbzKVs3lnA5Zvejs'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_2 as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_1 as string
+
+if (!supabaseAnonKey) {
+  throw new Error("NEXT_PUBLIC_SUPABASE_1 environment variable is not set.");
+}
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
